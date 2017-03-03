8899c27669b4dfdc8d94934ecb7afba3ac663a4e
Rameş Aliyev <creator@ramesaliyev.com>
2017-03-04 00:52:07 +0300

    Add primitive type examples

---
+++ code_samples/02_Types/src/CharAndBoolean.java
@@ -0,0 +1,13 @@
+public class CharAndBoolean {
+    public static void main (String[] args) {
+        // PRIMITIVE DATA TYPES
+
+        // char
+        // 16 bit
+        char myChar = 'a';
+        char myUnicodeChar = '\u00A9';
+
+        // boolean
+        boolean myBoolean = true;
+    }
+}
---
+++ code_samples/02_Types/src/TheString.java
@@ -0,0 +1,10 @@
+public class TheString {
+    public static void main (String[] args) {
+        // PRIMITIVE DATA TYPES
+
+        // STRING
+
+        String myString = "Vay be ulan be!";
+
+    }
+}
